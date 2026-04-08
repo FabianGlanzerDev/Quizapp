070bba0d03e030402edbe628f859f4489c4fa706
Fabian Glanzer <Fabian.glanzer99@gmx.at>
2026-04-08 15:39:58 +0200

    add sounds and finish project

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 let questions = [
     {
-        "question": "Wer hat HTML erfunden ?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
+        "question": "Wann kam Yvi zur Welt ?",
+        "answer_1": "12.05.1996",
+        "answer_2": "10.07.1996",
+        "answer_3": "14.06.1998",
+        "answer_4": "10.07.1996",
+        "right_answer": 2
+    },
+    {
+        "question": "Zur welcher Uhrzeit wurde Yvi geboren ?",
+        "answer_1": "06:01 Uhr",
+        "answer_2": "05:53 Uhr",
+        "answer_3": "05:51 Uhr",
+        "answer_4": "05:49 Uhr",
         "right_answer": 3
     },
     {
-        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": "readonly",
-        "answer_2": "max",
-        "answer_3": "from",
-        "answer_4": "spellcheck",
-        "right_answer": 1
+        "question": "Wann hat Fabs die LAP ERFOLGREICH abgeschlossen ?",
+        "answer_1": "September 2018",
+        "answer_2": "garnicht",
+        "answer_3": " September 2020",
+        "answer_4": " September 2019",
+        "right_answer": 4
     },
     {
-        "question": "Welches FF Fahrzeug hat eine ausfahrbare Leiter?",
-        "answer_1": "TLF",
-        "answer_2": "HLF",
-        "answer_3": "DLK",
-        "answer_4": "Boot",
-        "right_answer": 3
+        "question": "Aus welchem Material ist Yvis Rücken",
+        "answer_1": "Aluminium",
+        "answer_2": "Titan oder Kobalt-Chrom-Legierungen",
+        "answer_3": "Eisen",
+        "answer_4": "Holz",
+        "right_answer": 2
     },
-    {
-        "question": "Welche Turbine hat den Kondensationsteil?",
-        "answer_1": "DT4",
-        "answer_2": "DT3",
-        "answer_3": "KZ1",
-        "answer_4": "KZ2",
-        "right_answer": 1
-    },
+
 
 ];
 
+let rightQuestions = 0;
 let currentQuestion = 0;
+let audio = new Audio('audio/audio.mp3');
+let audioFALSCH = new Audio('audio/falsch.mp3');
 
 function init() {
     document.getElementById('all-questions').innerHTML = questions.length
@@ -47,7 +51,17 @@ function showQuestion() {
     if (currentQuestion >= questions.length) {
         document.getElementById('endScreen').style = '';
         document.getElementById('questionBody').style = 'display:none';
-    } else {
+
+        document.getElementById('amount-of-questions').innerHTML = questions.length;
+        document.getElementById('amount-of-right-questions').innerHTML = rightQuestions;
+    } else { //show next question
+
+        let percent = (currentQuestion + 1) / questions.length;
+        percent = Math.round(percent * 100);
+        document.getElementById('progress-bar').innerHTML = `${percent} %`;
+        document.getElementById('progress-bar').style.width = `${percent}%`;
+
+
         let question = questions[currentQuestion];
 
         document.getElementById('question-number').innerHTML = currentQuestion + 1;
@@ -66,9 +80,12 @@ function answer(selection) {
 
     if (selectedQuestionNumber == question['right_answer']) {
         document.getElementById(selection).parentNode.classList.add('bg-success');
+        audio.play();
+        rightQuestions++;
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        audioFALSCH.play();
     }
     document.getElementById('next-button').disabled = false;
 }
@@ -91,4 +108,6 @@ function resetAnswerButton() {
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 }
+
+
 
