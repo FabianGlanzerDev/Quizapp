63337cd1ac7f228139525283563160f8cd4c876c
Fabian Glanzer <Fabian.glanzer99@gmx.at>
2026-04-08 13:34:19 +0200

    add logic javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let questions = [
     {
-        "question": "Wer hat HTML erfunden?",
+        "question": "Wer hat HTML erfunden ?",
         "answer_1": "Robbie Williams",
         "answer_2": "Lady Gaga",
         "answer_3": "Tim Berners-Lee",
@@ -15,28 +15,80 @@ let questions = [
         "answer_4": "spellcheck",
         "right_answer": 1
     },
-    
+    {
+        "question": "Welches FF Fahrzeug hat eine ausfahrbare Leiter?",
+        "answer_1": "TLF",
+        "answer_2": "HLF",
+        "answer_3": "DLK",
+        "answer_4": "Boot",
+        "right_answer": 3
+    },
+    {
+        "question": "Welche Turbine hat den Kondensationsteil?",
+        "answer_1": "DT4",
+        "answer_2": "DT3",
+        "answer_3": "KZ1",
+        "answer_4": "KZ2",
+        "right_answer": 1
+    },
+
 ];
 
 let currentQuestion = 0;
 
-function init(){
-document.getElementById('all-questions').innerHTML = questions.length
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length
 
-showQuestion();
+    showQuestion();
 }
 
 function showQuestion() {
-let question = questions[currentQuestion];
 
-document.getElementById('questiontext').innerHTML = question['question'];
-document.getElementById('answer_1').innerHTML = question['answer_1'];
-document.getElementById('answer_2').innerHTML = question['answer_2'];
-document.getElementById('answer_3').innerHTML = question['answer_3'];
-document.getElementById('answer_4').innerHTML = question['answer_4'];
+    if (currentQuestion >= questions.length) {
+        document.getElementById('endScreen').style = '';
+        document.getElementById('questionBody').style = 'display:none';
+    } else {
+        let question = questions[currentQuestion];
+
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questiontext').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
 }
 
-function answer(selection){
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-
+    if (selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+    }
+    document.getElementById('next-button').disabled = false;
 }
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButton();
+    showQuestion();
+}
+
+function resetAnswerButton() {
+
+    document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-success');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
